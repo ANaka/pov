3d364eb1e0998b8b66a7432ec6c6d682daf4f3ea
anaka <alex.naka@gmail.com>
2021-06-06 11:40:52 -0700

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='pov',
+      version='1.0',
+      description='simple image/video capture',
+      author='Alex Naka',
+      author_email='alex.naka@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=find_packages(),
+     )
